4df61dc5caf653bcbe124504d1b406ad7d0efd90
Dávid Németh <david.nemeth90@gmail.com>
2016-05-21 07:48:40 +0200

    Update OuterJoin.sql

--- OuterJoin.sql
+++ OuterJoin.sql
@@ -2,7 +2,8 @@ use NORTHWND
 
 select *
 
-from Customers c left outer join orders o
+from Customers c left join orders o
 	on c.CustomerID = o.CustomerID
-	
+
+where o.OrderID is null
 order by o.CustomerID
