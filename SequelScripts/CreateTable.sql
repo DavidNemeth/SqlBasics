08529c8297ded2c459b83ac0a6a1393b90c82946
DavidNemeth <david.nemeth90@gmail.com>
2016-05-23 15:58:01 +0200

    Constraints
    added constraints

--- SequelScripts/CreateTable.sql
+++ SequelScripts/CreateTable.sql
@@ -1,15 +1,20 @@
-if(OBJECT_ID('Cows') is not null)
-	drop table Cows
+if(OBJECT_ID('PubMembers') is not null)
+	drop table PubMembers
 go
 
-create table Cows (
+create table PubMembers (
 	ID int primary key identity(1,1),
-	Name varchar(50),
-	MooCount int
+	FirstName varchar(100) not null,
+	LastName varchar(100) not null,
+	startdate datetime not null,
+	enddate datetime null,
+	Age int constraint LegalAge check(Age >= 18),
+	
+	check(startdate < enddate)
 	)
 
-insert into Cows values('Betsy', 10)
+insert into PubMembers values('Willy', 'Wonka', GETDATE(), DATEADD(year, 1, getdate()), 30)
 
 
 select *
-from cows
+from PubMembers
